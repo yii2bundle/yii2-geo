c29fa80f7256ce2f47e0677e2e4415ee8a244c84
Vitaliy <theyamshikov@yandex.ru>
2019-02-07 00:11:38 +0600

    refacto

--- src/domain/validators/PhoneValidator.php
+++ src/domain/validators/PhoneValidator.php
@@ -5,10 +5,12 @@ namespace yii2lab\geo\domain\validators;
 use App;
 use yii\base\InvalidArgumentException;
 use yii\validators\Validator;
+use yii2lab\geo\domain\helpers\PhoneHelper;
 
 class PhoneValidator extends Validator  {
 	
 	public function validateAttribute($model, $attribute) {
+        $model->$attribute = PhoneHelper::clean($model->$attribute);
 		try {
 			App::$domain->geo->phone->validate($model->$attribute);
 		} catch(InvalidArgumentException $e) {
