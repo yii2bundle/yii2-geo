cef0215791b67b00ac7667899b1c23cb58e5e7af
Vitaliy <theyamshikov@yandex.ru>
2019-02-09 22:00:25 +0600

    fix

--- src/domain/services/CurrencyValueService.php
+++ src/domain/services/CurrencyValueService.php
@@ -2,18 +2,25 @@
 
 namespace yii2lab\geo\domain\services;
 
+use yii2lab\geo\domain\entities\CurrencyValueEntity;
 use yii2rails\domain\data\Query;
 use yii2rails\domain\services\base\BaseActiveService;
+use yii2rails\domain\values\TimeValue;
 
 class CurrencyValueService extends BaseActiveService {
 	
 	public function all(Query $query = null) {
 		$query = Query::forge($query);
-		$query->andWhere(['publicated_at' => date('Y-m-d 00:00:00')]);
+		$timeValue = new TimeValue;
+		$timeValue->setFromFormat(date('Y-m-d 00:00:00'), TimeValue::FORMAT_WEB);
+		//prr($timeValue->getInFormat(TimeValue::FORMAT_WEB),1,1);
+		$query->andWhere(['publicated_at' => $timeValue->getInFormat(TimeValue::FORMAT_WEB)]);
+		/** @var CurrencyValueEntity[] $collection */
 		$collection = parent::all($query);
 		if(empty($collection)) {
 			$collection = $this->domain->repositories->currencyTransfer->all();
 			foreach($collection as $entity) {
+                $entity->publicated_at = $timeValue;
 				$this->repository->insert($entity);
 			}
 		}
